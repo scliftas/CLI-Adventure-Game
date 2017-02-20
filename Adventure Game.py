cbd608cc53ab16819a11600ca1ce9aba4739c1d1
scliftas <scliftas@users.noreply.github.com>
2017-02-20 22:29:21 +0000

    GZip feature added

--- Adventure Game.py
+++ Adventure Game.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import random
 import time
 
@@ -15,6 +17,14 @@ def Bin():
     CurrentDirectory = ("/bin")
     global contents
     contents = [""]
+
+def Logs():
+    global PreviousDirectory
+    PreviousDirectory = ("FileSystem")
+    global CurrentDirectory
+    CurrentDirectory = ("/logs")
+    global contents
+    contents = [LogFile.name]
 
 def Home():
     global PreviousDirectory
@@ -69,29 +79,29 @@ class File:
 
     #Virus count can only go up to 1, meaning there can only be one virus source file
     VirusCount = 0
+    VirusPath = ""
 
     #File object generator
-    def __init__(self, name, path, content):
-        self.name = name
-        self.path = path
-        self.content = content
-        self.protected = random.choice([True, False])
-        self.encrypted = random.choice([True, False])
-        self.compressed = random.choice([True, False])
+    def __init__(self, name, path, content, compressed, potentVirus):
+        self.name = str(name)
+        self.path = str(path)
+        self.content = str(content)
+        self.compressed = bool(compressed)
+        self.potentVirus = bool(potentVirus)
 
         #Randomly generate whether or not the file is the virus source file
-        self.generateVirus = random.choice([True, False])
-        if self.generateVirus == True:
-            if File.VirusCount == 0:
-                self.virus = random.choice([True, False])
-                if self.virus == True:
-                    File.VirusCount = 1
+        if self.potentVirus == True:
+            self.generateVirus = random.choice([True, False])
+            if self.generateVirus == True:
+                if File.VirusCount == 0:
+                    self.virus = random.choice([True, False])
+                    if self.virus == True:
+                        File.VirusCount = 1
+                        File.VirusPath = (self.path+self.name)
 
     def displayProps(self):
         print("Name: ", self.name)
         print("File path: ", self.path)
-        print("Protected: ", self.protected)
-        print("Encrypted: ", self.encrypted)
         print("Compressed: ", self.compressed)
         print("Generate Virus: ", self.generateVirus)
         if self.generateVirus == True:
@@ -101,11 +111,23 @@ class File:
         print(self.content)
 
 
-DocumentsOne = File("hello_world.txt", "/home/documents/", "Hello world")
+LogFile = File("virus_report.txt", "/logs/", "System found an error: exception 302.\nWe're not sure what this is, but it could mean your system is infected. \n \nPerhaps try finding the source file that generated the error?", False, False)
+DocumentsOne = File("hello_world.txt", "/home/documents/", "Hello world", False, True)
 
 #Bash commands
 
-#View contents of folder or file command
+#View contets of folder or properties of file command
+def gzip(Input):
+    Dir = Input.split("gzip ", 1)[1]
+    compDir = folder_dict[Dir]
+    if compDir.compressed == True:
+        compDir.compressed = False
+        print("Uncompressed")
+    elif compDir.compressed == False:
+        print("This folder is not compressed!")
+    else:
+        print("bash: gzip:"+Dir+": No such compressed directory")
+
 def ls(Input):
     Dir = Input.split("ls ", 1)[1]
     if "-l" not in Input:
@@ -127,12 +149,14 @@ def cd(Input):
     else:
         print("bash: cd:"+CDDir+": No such file or directory")
 
-#View properties of file command
+#View contents of file command
 def cat(Input):
     CatFile = Input.split("cat ", 1)[1]
     if CatFile in file_dict:
         FileContent = file_dict[CatFile].displaycontent
+        print("")
         FileContent()
+        print("")
     else:
         print("cat: "+CatFile+": No such file or directory")
 
@@ -150,8 +174,9 @@ def rm(Input):
 
 #Original commands
 def help(Input):
-    print('\033[1m'+"Commands:"+'\033[0m'+"\n - ls [directory] to display folder contents \n - ls -l [directory] to view file properties \n - cd [directory] to move to a different folder "+
-                                          "\n - cat [directory] to view file contents \n - rm [directory] to delete a file \n - exit to exit the game")
+    print("Commands:"+"\n - ls [directory] to display folder contents \n - ls -l [directory] to view file properties \n - cd [directory] to move to a different folder "+
+                                          "\n - cat [directory] to view file contents \n - rm [directory] to delete a file \n - exit to exit the game" +
+                                          "\n - gzip [directory] to uncompress a folder")
 
 def win():
 
@@ -179,7 +204,7 @@ def win():
 
     #Decrypting scoreboard.txt and storing lines to array
     scoreboard = open("scoreboard.txt", "r")
-    print("\n"+'\033[1m'+"SCOREBOARD:")
+    print("SCOREBOARD:")
     fileContents = []
     for line in scoreboard:
         line = [ord(i)for i in line]
@@ -190,35 +215,42 @@ def win():
     scoreboard.close()
 
     #Printing scoreboard.txt array as a sorted list
-    fileContSort = sorted(fileContents, reverse=True)
+    fileContSort = sorted(fileContents, reverse =True)
     fileContSort = '\n'.join(fileContSort)
     print(fileContSort)
 
 #Exit function to close game
 def gameExit(Input):
     exit()
+
 #Main Program
 start = time.time()
 
 #Custom dictionaries linking phrases to functions
 command_dict = {"ls":ls, "help":help, "cd":cd, "cat":cat, "rm":rm, "exit":gameExit}
-folder_dict = {"FileSystem": FileSystem, "/home":Home, "/bin":Bin, "/home/documents":Documents, "/home/downloads":Downloads, "/home/music":Music, "/home/pictures":Pictures, "/home/videos":Videos}
-file_dict = {"/home/documents/hello_world.txt":DocumentsOne}
+folder_dict = {"FileSystem": FileSystem, "/home":Home, "/bin":Bin, "/home/documents":Documents, "/home/downloads":Downloads, "/home/music":Music, "/home/pictures":Pictures, "/home/videos":Videos, "/log":Logs}
+file_dict = {"/home/documents/hello_world.txt":DocumentsOne, "/logs/virus_report.txt":LogFile}
 
-DocumentsOne.displayProps()
+print(File.VirusPath)
 
-Username = (input(str('\033[1m'+"Please enter your username: ")))
+Username = (input(str("Please enter your username: ")))
 CurrentDirectory = ("")
 contents = [""]
+
+print("WARNING: Potential virus detected! Report saved to log file. Please see /logs/virus_report.txt")
 
 FileSystem()
 
 while True:
 
     #Display command line input area
-    UserInput = (input(str('\033[1m'+Username+"@System:"+CurrentDirectory+"$ ")))
+    UserInput = (input(str(Username+"@System:"+CurrentDirectory+"$ ")))
     Input = UserInput.lower()
 
     #Shorten the input to the first word, making it a command, then referencing dictionaries
     Command = Input.split(" ", 1)[0]
-    command_dict[Command](Input)
+    if Command in command_dict:
+        command_dict[Command](Input)
+
+    elif Command not in command_dict:
+        print("Invalid command!")
